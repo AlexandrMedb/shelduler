f30bad28c6e3c48c5dc379c0096c9fe5c7e8c6f6
Alexandr <torasov.alexandr@gmail.com>
2022-04-08 16:43:47 +0300

    change schedule tp calendar

--- src/components/adminPanel.tsx
+++ src/components/adminPanel.tsx
@@ -11,9 +11,9 @@ export default function AdminPanel() {
   return (
     <List sx={{width: 200, borderRight: '1px solid lightgray', minHeight: '95vh',
       boxSizing: 'borderBox'}}>
-      <NavBarItem icon={<CalendarMonthIcon/>} link={'/'} text={'schedule'}/>
-      <NavBarItem icon={<RoomPreferencesIcon/>} link={'/rooms'} text={'rooms'}/>
-      <NavBarItem icon={<GroupIcon/>} link={'/users'} text={'users'}/>
+      <NavBarItem icon={<CalendarMonthIcon/>} link={'/'} text={'Calendar'}/>
+      <NavBarItem icon={<RoomPreferencesIcon/>} link={'/rooms'} text={'Rooms'}/>
+      <NavBarItem icon={<GroupIcon/>} link={'/users'} text={'Users'}/>
     </List>
   );
 }
@@ -29,7 +29,7 @@ export const NavBarItem = ({text, icon, link}:props) => {
     <Link to={link} style={{color: 'rgba(0,0,0,0.72)', textDecoration: 'none'}}>
       <ListItem sx={{p: 1}}>
         {icon}
-        <ListItemText sx={{ml: 2, textTransform: 'uppercase'}}
+        <ListItemText sx={{ml: 2}}
           primary={text}
         />
       </ListItem>
